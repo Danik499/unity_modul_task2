222b59b8a2882fa6cd3205d299863383b5713dbb
Danik499 <danikshcherban499@gmail.com>
2020-10-16 19:18:24 +0300

    modul

---
+++ Assets/Normal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Normal : MonoBehaviour
+{
+    private Vector3 v1, v2;
+    void Start()
+    {
+        v1 = new Vector3(1, 5, -7);
+        v2 = new Vector3(2, 0, -6);
+    }
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Vector3.Cross(v1, v2));
+        }
+    }
+}
